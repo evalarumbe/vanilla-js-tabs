003311566adaeb54518f1b9d3dc4b81aaf7476ea
Eva Larumbe <evalarumbe@gmail.com>
2020-12-29 07:14:25 +0100

    Complete JS refactor - now works independently from its previous context

--- tabs.js
+++ tabs.js
@@ -19,55 +19,43 @@
  * The first tab is active by default, but this can be overridden by including these classes in the HTML.
  */
 
-// TODO: Wait til the window loads before setting up tabs
-// window.addEventListener('load', () => { handleTabs(currentPostContainer); });
-
-function handleTabs(currentPostContainer) {
+function loadTabs() {
   'use strict';
   
   // Function definitions
+  
   function init() {
-    // Check for multiple parent Tabs elements in this post
-    const tabElements = [...currentPostContainer.querySelectorAll('.tabpanel')];
+    // Check for multiple containers with tabs
+    const containers = [...document.querySelectorAll('.tab-container')];
 
-    // If there are Tabs elements, set up their nested tabs
-    tabElements.length > 0 && tabElements.forEach(parentTabsElement => {
-      // If applicable, set up nested tabs within the active parent tab
-      initParentTabs(parentTabsElement);
-    });
+    // If there are tab containers, set up their tabs
+    containers.length > 0 && containers.forEach(container => new TabContainer(container));
   }
 
-  function initParentTabs(parentTabsElement) {
-    // Get tab controls from DOM
-    const parentTabCtrls = [...parentTabsElement.querySelectorAll('.tab_titles [role=tab]')];
-
-    // Run the ParentTab constructor for each control, which sets up its nested tabs
-    parentTabCtrls.forEach(tabCtrl => new ParentTab(tabCtrl));
-  }
-
-  function initTabs(parentTab) {
-    // Create Nested Tab instances from nested controls found in this parent panel
-    const tabCtrls = [...parentTab.panel.querySelectorAll('.foh-nested-tab-controls [role=tab]')];
-    const tabs = tabCtrls.map(tabCtrl => new Tab(tabCtrl, parentTab));
+  function initTabs(tabContainer) {
+    // Create Tab instances from controls found in this parent panel
+    const tabCtrls = [...tabContainer.node.querySelectorAll('.tab-controls [role=tab]')];
+    const tabs = tabCtrls.map(tabCtrl => new Tab(tabCtrl, tabContainer));
     
-    // Check if any of the controls have been marked as active with a class by the user
+    // Check if any of the controls have already been marked as active
+    // (this allows the initial active tab to optionally be set in the HTML)
     const activeCtrls = tabCtrls.filter(ctrl => ctrl.classList.contains('active-tab-control'));
 
     let initialActiveTab; // will contain an instance of Tab
 
     if (activeCtrls.length) {
-      // Set the active nested tab to the first marked as active in the DOM
+      // Set the active tab to the first marked as active in the DOM
       initialActiveTab = tabs.filter(tab => tab.ctrl === activeCtrls[0])[0];
 
       // Warn the user if more than one tab control is marked as active
       if (activeCtrls.length > 1) {
-        throw new Error("Multiple nested tab controls were found with the class 'active-tab-control'. Only one was expected, so the first has been marked active by default.");
+        throw new Error("Multiple tab controls were found with the class 'active-tab-control'. Only one was expected, so the first has been marked active by default.");
       }
     } else if (tabCtrls.length) {
-      // If none are marked as active, set the first one to active by default
+      // If tabs are present but none are marked as active, set the first one to active by default
       initialActiveTab = tabs.filter(tab => tab.ctrl === tabCtrls[0])[0];
     } else {
-      // no nested tabs are present
+      // no tabs are present
       return [];
     }
 
@@ -80,22 +68,21 @@ function handleTabs(currentPostContainer) {
     });
 
     // Keep track of which tab to deactivate if/when another becomes active
-    parentTab.activeTab = initialActiveTab;
+    tabContainer.activeTab = initialActiveTab;
 
     return tabs;
   }
 
-  // // Prototype definitions
-  // function ParentTab(tabCtrl) {
-  //   // Declare members
-  //   this.ctrl = tabCtrl;
-  //   this.panel = document.querySelector(`#${this.ctrl.getAttribute('aria-controls')}`);
-  //   this.tabs = initTabs(this);
-  //   this.activeTab; // will contain an instance of Tab
-  //   this.prevActiveTab; // will contain an instance of Tab
-  // }
+  // Prototype definitions
+  function TabContainer(container) {
+    // Declare members
+    this.node = container;
+    this.tabs = initTabs(this);
+    this.activeTab; // will contain an instance of Tab
+    this.prevActiveTab; // will contain an instance of Tab
+  }
 
-  function Tab(tabCtrl, parentTab) {
+  function Tab(tabCtrl, tabContainer) {
     this.ctrl = tabCtrl;
     this.panel = document.querySelector(`#${tabCtrl.getAttribute('aria-controls')}`);
 
@@ -103,22 +90,22 @@ function handleTabs(currentPostContainer) {
     this.activate = (function() {
       // If there's already an active tab (i.e. this is not the first load),
       // Mark it as 'previous' so it can be deactivated
-      if (parentTab.activeTab) {
-        parentTab.prevActiveTab = parentTab.activeTab;
+      if (tabContainer.activeTab) {
+        tabContainer.prevActiveTab = tabContainer.activeTab;
       }
 
-      // Display nested tab content
+      // Display tab content
       this.panel.classList.add('active-tab-panel');
 
-      // Update nested tab navigation
+      // Update tab navigation
       this.ctrl.classList.add('active-tab-control');
       this.ctrl.removeEventListener('click', this.activate); // if not present, this does nothing
 
       // If there was a previously active tab, deactivate it
-      parentTab.prevActiveTab && parentTab.prevActiveTab.deactivate();
+      tabContainer.prevActiveTab && tabContainer.prevActiveTab.deactivate();
 
       // Keep track of which tab to deactivate if/when another becomes active
-      parentTab.activeTab = this;
+      tabContainer.activeTab = this;
 
     // Bind so the func can be used as an EventListener,
     // with 'this' still referring to the Tab instance (not the EventTarget)
@@ -138,3 +125,5 @@ function handleTabs(currentPostContainer) {
     console.warn(error);
   }
 }
+
+window.addEventListener('load', () => { loadTabs(); });
